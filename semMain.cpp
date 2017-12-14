7bb889f844c61324adb40a16ce8d8398ffd4efe2
Adam01010101F <32944666+Adam01010101F@users.noreply.github.com>
2017-12-13 16:50:41 -0800

    Update semMain.cpp

--- semMain.cpp
+++ semMain.cpp
@@ -14,16 +14,19 @@
 #include <vector>
 #include "semaphore.h"
 
-int getLoc(vector<long>, long);
-
-enum {uSem, vSem};
 using namespace std;
+
+int getLoc(vector<long>, long);
+enum {uSem, vSem};
+
 
 int main(){
      srand(time(NULL));
-     SEMAPHORE sem(2);
-     //sem.V(uSem);
-     //sem.V(vSem);
+     int two  = 2;
+     int qid = 0;
+     SEMAPHORE sem(two);//= new SEMAPHORE(two);
+     sem.V(uSem);
+     sem.V(vSem);
      int num =-1;
      long parentPID = getpid();
      vector<long> childPID;
@@ -34,12 +37,13 @@ int main(){
      }while(getpid()==parentPID && childPID.size()<4);
      
      if(getpid()==parentPID){
-          cout<<"I am the parent"<<endl;          
+          createConnection();         
      }
      else{
           cout<<num<<": ";
           if(num %2 ==0){          
                int U = 827395609;
+               sem.P(uSem);
                while(1){
                     int R = rand();
                     if(R<100 || R%U==0){
@@ -48,9 +52,12 @@ int main(){
                          cout<<num<<": "<<U<<"/"<<R<<" = "<<U/R
                                   <<endl;}
                }
+               sem.V(uSem);
+               
           }
           else{
                int V = 962094883;
+               sem.P(vSem);
                while(1){
                     int R = rand();
                     if(R<100 || R%V==0){
@@ -60,11 +67,12 @@ int main(){
                                   <<endl;
                     }
                }
+               sem.V(vSem);
           }
      }
           
      
-          
+    return 0;  
 }
 
 int getInt(){
@@ -79,3 +87,55 @@ int getLoc(vector<long> cPID, long target){
      }
      return -1;
 }
+
+
+/**
+ * Alerts Sender 251/997 that the msgQ is established
+ **/
+ int estConnection(){
+    //Creating msgQ
+    std::cout<<"Establishing Connection...\n";
+    int qid = msgget(ftok(".",'u'), 0);
+    buf msg;
+    int size = sizeof(msg) - sizeof(long);
+    
+    //Sending est. msgs
+    msg.mtype = 251;
+    msg.content = 711;
+    msgsnd(qid, (struct buf*)&msg, size, 0);
+    msg.mtype=997;
+    msgsnd(qid, (struct buf*)&msg, size, 0);
+    
+    //Receiving est. msgs
+    msgrcv(qid, (struct buf*)&msg, size, 512,0);
+    std::cout<<"Transmitter 251 Found...\n";
+    msgrcv(qid, (struct buf*)&msg, size, 513,0);
+    std::cout<<"Transmitter 997 Found...\n"; 
+
+    return qid;
+ }
+ 
+ /**
+ * Alerts Sender 257/997 that the msgQ is established
+ **/
+ int createConnection(){
+    std::cout<<"Establishing Connection...";
+    int qid = msgget(ftok(".",'u'), IPC_EXCL|IPC_CREAT|0600);
+    buf msg;
+    int size = sizeof(msg) - sizeof(long);
+    
+    //Sending est. msgs
+    msg.mtype = 257;
+    msg.content = 711;
+    msgsnd(qid, (struct buf*)&msg, size, 0);
+    msg.mtype=997;
+    msgsnd(qid, (struct buf*)&msg, size, 0);
+    
+    //Receiving est. msgs
+    msgrcv(qid, (struct buf*)&msg, size, 1024,0);
+    std::cout<<"Transmitter 257 Found..\n";
+    msgrcv(qid, (struct buf*)&msg, size, 1025,0); 
+    std::cout<<"Transmitter 997 Found..\n";
+
+    return qid;
+ }
